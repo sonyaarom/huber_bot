31fc96145ca5d5e6b8292c724e032c1c0a2bd8c1
Sofia <74678718+sonyaarom@users.noreply.github.com>
2024-09-26 16:06:37 +0200

    Update config.py

--- src/retriever_evaluation/vector_db_retrieval_eval/config.py
+++ src/retriever_evaluation/vector_db_retrieval_eval/config.py
@@ -6,25 +6,16 @@ load_dotenv()
 
 # API Configurations
 API_CONFIGS = {
-    # "all-mini-eucl": {
-    #     "api_key": '2e55369e-62f2-4317-8273-faf282511d2b',
-    #     "default_embedding_model": "sentence-transformers/all-mpnet-base-v2"
-    # },
-    "all-mini-cosine": {
-        "api_key": '2966f5ad-7763-45be-a7ab-7c24afc01ff7',
-        "default_embedding_model": "all-MiniLM-L6-v2"
-    # },
-    # "all-mini-dotproduct": {
-    #     "api_key": 'b4f8e710-fc88-4b6a-a2b8-433f60359b10',
-    #     "default_embedding_model": "all-MiniLM-L6-v2"
-    }
-
+    "PROJECT_NAME": {
+        "api_key": API_KEY,
+        "default_embedding_model": "sentence-transformers/all-mpnet-base-v2"
+    },
     
 }
 
 # File paths
-QA_DF_PATH = '/Users/s.konchakova/Thesis/assets/csv/qa_df.csv'
-BM25_VALUES_PATH = '/Users/s.konchakova/Thesis/assets/bm25_values.json'
+QA_DF_PATH = ''
+BM25_VALUES_PATH = ''
 
 # NER settings
 NER_MODEL = "urchade/gliner_small-v2.1"
