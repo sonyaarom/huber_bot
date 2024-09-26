63c960b2f27b5c5fd1cff3220a7f09e3cc520638
Sofia Konchakova <s.konchakova@dcmn.com>
2024-09-26 16:04:33 +0200

    updated config.py

--- src/retriever_evaluation/vector_db_retrieval_eval/config.py
+++ src/retriever_evaluation/vector_db_retrieval_eval/config.py
@@ -6,21 +6,11 @@ load_dotenv()
 
 # API Configurations
 API_CONFIGS = {
-    # "all-mini-eucl": {
-    #     "api_key": '2e55369e-62f2-4317-8273-faf282511d2b',
-    #     "default_embedding_model": "sentence-transformers/all-mpnet-base-v2"
-    # },
-    "all-mini-cosine": {
+    "": {
         "api_key": '2966f5ad-7763-45be-a7ab-7c24afc01ff7',
         "default_embedding_model": "all-MiniLM-L6-v2"
-    # },
-    # "all-mini-dotproduct": {
-    #     "api_key": 'b4f8e710-fc88-4b6a-a2b8-433f60359b10',
-    #     "default_embedding_model": "all-MiniLM-L6-v2"
-    }
-
     
-}
+}}
 
 # File paths
 QA_DF_PATH = '/Users/s.konchakova/Thesis/assets/csv/qa_df.csv'
